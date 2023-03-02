d86f24888f99c807803f3b1276cf6aa7a708c1b4
Acteonis <adosminkina@gmail.com>
2023-03-02 03:41:18 +0000

    the lab is completed

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+/*  * In problem1.sql, write a SQL statement that adds a "lastname" column to the site_user table, of type
+     * varchar(100).*/
+     alter table site_user add lastname varchar(100);
